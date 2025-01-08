45241cf88e8d0b02e89db5af8a86a9c4ca7bf37c
aksh-1h <narwaniaksh57@gmail.com>
2025-01-08 17:22:18 +0530

    Initial Commit

---
+++ project123/project123App.swift
@@ -0,0 +1,17 @@
+//
+//  project123App.swift
+//  project123
+//
+//  Created by student on 08/01/25.
+//
+
+import SwiftUI
+
+@main
+struct project123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
